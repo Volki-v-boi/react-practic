4e22f6f954558e26771130089f0a6ff919c5f8d1
Volki-v-boi <152771192+Volki-v-boi@users.noreply.github.com>
2025-09-14 13:33:28 +0200

    fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,17 @@
-import { useSelector } from "react-redux";
-import Balance from "../Balance/Balance";
-import LangSwitcher from "../LangSwitcher/LangSwitcher";
+// import { useSelector } from "react-redux";
+// import Balance from "../Balance/Balance";
+// import LangSwitcher from "../LangSwitcher/LangSwitcher";
 
 export default function App() {
-  const lang = useSelector((state) => state.locale.lang);
+  // const lang = useSelector((state) => state.locale.lang);
   return (
     <div>
-      <h1>Credits</h1>
+      {/* <h1>Credits</h1>
       <Balance />
       <hr />
       <LangSwitcher />
-      <p>Selected lang: {lang} </p>
+      <p>Selected lang: {lang} </p> */}
+      {/* ___________practic 2____________ */}
     </div>
   );
 }
